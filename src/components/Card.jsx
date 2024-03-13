052e3b0cc8ef4bcbb41d641c8ea403196ebbb47c
Pedro H. Marques <phmarquesdev@gmail.com>
2024-03-12 23:10:50 -0300

    Add plan card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ plan }) => {
+  const imageUrl =
+    "https://www.civitatis.com/blog/wp-content/uploads/2022/10/panoramica-rio-janeiro-brasil.jpg";
+  return (
+    <div className="flex w-full cursor-pointer flex-col items-start justify-center gap-y-2 bg-white p-6">
+      <img
+        src={imageUrl}
+        alt="Imagem"
+        className="w-full rounded-md hover:scale-105"
+      />
+      <h2 className=" text-lg hover:opacity-80">{plan.title}</h2>
+      <div className="flex gap-x-4">
+        <p className="text-sm text-gray-600"> {plan.startDate}</p>
+        <p className="text-sm text-gray-600"> {plan.endDate}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import PlanContext from "../context/PlanContext";
+import Card from "../components/Card";
 
 const HomePage = () => {
+  const { plans } = useContext(PlanContext);
   return (
     <div className="mx-auto max-w-screen-lg py-10">
-      <div className="flex w-full flex-col items-start space-x-0 space-y-5 md:flex-row md:items-center md:justify-between md:space-x-5 md:space-y-0">
+      <div className="mb-10 flex w-full flex-col items-start space-x-0 space-y-5 md:flex-row md:items-center md:justify-around md:space-x-5 md:space-y-0 lg:justify-between">
         <h1 className="text-3xl font-semibold">My planned vacation trips</h1>
         <button className="rounded-full bg-primary px-5 py-2 text-white hover:bg-primary/65">
           + Plan holiday
         </button>
+      </div>
+      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {plans.map((plan) => (
+          <Card key={plan.id} plan={plan} />
+        ))}
       </div>
     </div>
   );
